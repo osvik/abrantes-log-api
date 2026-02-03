cb2c7fe3dd19b6986da6c3f97cdbab1c5b2acb01
Osvaldo Gago <opgago@yahoo.com>
2026-02-03 18:00:50 +0100

    Comment in config.php with instructions on sqlite db path

--- config.php
+++ config.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 return [
+
     // List of allowed origins for CORS
     // If empty, Access-Control-Allow-Origin: * will be used
     // Example: ['https://example.com', 'https://app.example.com']
     'allowed_origins' => [],
+
+    // Path to SQLite database file
     'db_path' => __DIR__ . '/logs.db',
+    
 ];
